e47cce10e27135306fad02b43d8dc935afca2f56
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-03-06 23:17:03 +0900

    add react-loading when searching movie

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
+import ReactLoading from "react-loading";
 
 // 5d4aa999
 
@@ -10,12 +11,17 @@ const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=5d4aa999";
 const App = () => {
   const [query, setQuery] = useState(null);
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const searchMovies = async (title) => {
+    setIsLoading(true);
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
     setMovies(data.Search);
+
+    console.log(isLoading);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -48,13 +54,17 @@ const App = () => {
       </div>
 
       <div className="container">
-        {movies?.length > 0 ? (
+        {
+        isLoading === true
+        ? ( <ReactLoading /> )
+        : (movies?.length > 0 ? (
           movies.map((movie) => <MovieCard movie={movie} />)
         ) : (
           <div className="empty">
             <h2>No movies found</h2>
           </div>
-        )}
+        ))
+        }
       </div>
     </div>
   );
